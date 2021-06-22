82b7b1ca8ce473110244b781f29833bcaf864bc6
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-06-22 09:09:33 -0500

    Login/splash page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route} from "react-router-dom";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Route path="/" render={(rp) => <Login{...rp}/>}/>
     </div>
   );
 }
