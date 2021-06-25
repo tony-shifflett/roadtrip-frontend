184083ee46d258e8999dafeab3eba432d2aa0fc7
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-06-25 02:00:00 -0500

    app.js, trips page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 import {Route, Link, Switch} from "react-router-dom";
+import axios from 'axios'
 import Login from "./pages/Login";
 import Trips from "./pages/Trips";
-import React, {useEffect, useState} from 'react';
+
 
 
 
 function App() {
 
-const url = 'https://finalbackendcls.herokuapp.com'
+  
+  const [tripList, setTripList] = useState()
+
+  useEffect(()=>{
+    axios.get('https://finalbackendcls.herokuapp.com/trips/')
+      .then(res => {
+        setTripList(res.data)
+      })
+  },[]);
 
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" render={(rp) => <Login{...rp}/>}/>
-        <Route exact path="/mytrips" render={(rp) => <Trips{...rp}/>}/>
+        <Route exact path="/mytrips" render={(rp) => <Trips{...rp} tripList={tripList}/>}/>
       </Switch>
     </div>
   );
--- src/pages/Trips.js
+++ src/pages/Trips.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react"
 import { Link } from 'react-router-dom'
 import Trip from '../components/Trip'
+import axios from 'axios'
 
 
 
