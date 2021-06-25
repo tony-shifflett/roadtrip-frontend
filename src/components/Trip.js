c85b79d078805c5dc3471868eb3e4fedf842b17a
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-06-25 06:56:08 -0500

    Trip component

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup"
+import axios from 'axios'
 
-const Trip = (props)=>{
+const Trip = (props) => {
 
-    return(
-        <h1>Trip</h1>
+    const url ='https://finalbackendcls.herokuapp.com/trips'
+
+    const deleteHandler = (id) =>{
+        axios.delete( url +`/${id}`)
+
+        axios.get(devUrl)
+        .then(res => {
+          props.setTripList(res.data)
+        })
+        
+    }
+    
+    
+    return (
+        <Card style={{width: '18rem', margin:"0 auto 5rem auto"}}>
+            <h1>{props.trip.name}</h1>
+            <p>From {props.trip.startpoint} to {props.trip.endpoint}</p>
+            <ButtonGroup>
+                <Button size="sm" style={{marginRight: "5px"}} onClick={()=>{deleteHandler(props.trip.id)}}>Delete</Button>
+                <Button size="sm">Edit</Button>
+            </ButtonGroup>
+        </Card>
     )
-
-
 }
 
 export default Trip
